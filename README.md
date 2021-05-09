123e4b6cab0b34942f34e02ff63bd04a57ea1e3d
speedmann <424659+speedmann@users.noreply.github.com>
2021-05-09 17:15:23 +0200

    Initial commit

--- README.md
+++ README.md
@@ -1,2 +1,62 @@
 # prometheus-chia-exporter
 Prometheus exporter for several chia node statistics
+It's assumed that the full node, the harvester and the wallet run on the same system.
+```
+# HELP chia_netspace_total Current total netspace
+# TYPE chia_netspace_total gauge
+chia_netspace_total 3.555581383124895e+18
+# HELP chia_average_block_time Average time between blocks
+# TYPE chia_average_block_time gauge
+chia_average_block_time 31.812749003984063
+# HELP chia_block_height Current highest block
+# TYPE chia_block_height gauge
+chia_block_height 255143.0
+# HELP chia_sync_state Current sync state
+# TYPE chia_sync_state gauge
+chia_sync_state{chia_sync_state="synced"} 1.0
+chia_sync_state{chia_sync_state="syncing"} 0.0
+# HELP chia_wallet_balance Balance of wallets
+# TYPE chia_wallet_balance gauge
+chia_wallet_balance{id="1",name="Chia Wallet"} 1.899905e+012
+# HELP chia_plots_count Total plots farmed by harvester
+# TYPE chia_plots_count gauge
+chia_plots_count 130.0
+# HELP chia_plots_size Total plot size farmed by harvester
+# TYPE chia_plots_size gauge
+chia_plots_size 1.4148014850864e+013
+# HELP chia_farmed_amount Total XCH farmed by harvester
+# TYPE chia_farmed_amount gauge
+chia_farmed_amount 2e+012
+# HELP chia_farmed_last_block Last height a farm reward was farmed
+# TYPE chia_farmed_last_block gauge
+chia_farmed_last_block 206883.0
+# HELP chia_time_to_win Expected time to win 
+# TYPE chia_time_to_win gauge
+chia_time_to_win 7.99496e+06
+```
+
+# Requirements
+The exporter is menat to be run from your chia-blockchain venv. Additionally you need `prometheus-client` as specified in `requirements.txt`
+
+```
+cd chia-blockchain
+. ./activate
+cd ../prometheus-chia-exporter
+pip install -r requirements.txt
+```
+
+# Installation
+TODO: Systemd services
+Edit `self_hostname` in `prometheus-chia-exporter/chia-exporter.py` 
+
+# Usage
+Start the systemd service
+Import `grafana/dashboard.json` to your grafana
+Add prometheus config
+
+```
+scrape_configs:
+  - job_name: 'chia'
+    static_configs:
+      - targets: ['nodeip:8000']
+```
---
+++ prometheus-chia-exporter/chia-exporter.py
@@ -0,0 +1,102 @@
+from prometheus_client import start_http_server, Gauge, Enum
+import asyncio
+import time
+from chia.rpc.wallet_rpc_api import WalletRpcApi
+from chia.rpc.wallet_rpc_client import WalletRpcClient
+from chia.rpc.full_node_rpc_client import FullNodeRpcClient
+from chia.rpc.harvester_rpc_client import HarvesterRpcClient
+from chia.util.config import load_config
+from chia.util.default_root import DEFAULT_ROOT_PATH
+from chia.cmds.netspace_funcs import netstorge_async as w
+from chia.cmds.farm_funcs import get_average_block_time
+
+self_hostname="full node ip"
+
+net_config={"daemon_ssl":{"private_crt":"","private_key":""}}
+
+
+NETSPACE = Gauge('chia_netspace_total', 'Current total netspace')
+BLOCK_TIME = Gauge('chia_average_block_time', 'Average time between blocks')
+HEIGHT = Gauge('chia_block_height', 'Current highest block')
+SYNC_STATE = Enum('chia_sync_state', 'Current sync state', states=['synced','syncing'])
+BALANCE = Gauge('chia_wallet_balance', 'Balance of wallets', ["name","id"])
+PLOTS_TOTAL = Gauge('chia_plots_count', 'Total plots farmed by harvester')
+PLOTS_SIZE = Gauge('chia_plots_size', 'Total plot size farmed by harvester')
+FARMED_AMOUNT = Gauge('chia_farmed_amount', 'Total XCH farmed by harvester')
+FARMED_LAST = Gauge('chia_farmed_last_block', 'Last height a farm reward was farmed')
+TIME_TO_WIN = Gauge('chia_time_to_win', 'Expected time to win ')
+
+async def main():
+    try:
+        config = load_config(DEFAULT_ROOT_PATH, "config.yaml")
+        rpc_port = config["full_node"]["rpc_port"]
+        wallet_rpc_port = config["wallet"]["rpc_port"]
+        harvester_rpc_port = config["harvester"]["rpc_port"]
+        client = await WalletRpcClient.create(self_hostname, wallet_rpc_port,DEFAULT_ROOT_PATH,config)
+        client_node = await FullNodeRpcClient.create(self_hostname, rpc_port,DEFAULT_ROOT_PATH,config)
+        client_harvester = await HarvesterRpcClient.create(self_hostname, harvester_rpc_port,DEFAULT_ROOT_PATH,config)
+
+        # blockchain stuff
+        blockchain = await client_node.get_blockchain_state()
+        netspace = blockchain['space']
+        NETSPACE.set(netspace)
+        average_block_time = await get_average_block_time(rpc_port)
+        BLOCK_TIME.set(average_block_time)
+        status =blockchain['sync']['synced']
+        print(status)
+        if not status:
+            SYNC_STATE.state('syncing')
+        else:
+            SYNC_STATE.state('synced')
+        height = await client.get_height_info()
+        HEIGHT.set(height)
+
+        # wallet stuff
+        wallets = await client.get_wallets()
+        wallet_amounts = {}
+        for wallet in wallets:
+            balance = await client.get_wallet_balance(wallet['id'])
+            #wallet_amounts[wallet['name']] = balance['confirmed_wallet_balance']
+            BALANCE.labels(name=wallet['name'], id=wallet['id']).set(balance['confirmed_wallet_balance'])
+
+
+
+        # harvester stats
+        plots = await client_harvester.get_plots()
+        plot_count = len(plots['plots'])
+        PLOTS_TOTAL.set(plot_count)
+        plot_size_total = 0
+        for plot in plots['plots']:
+            plot_size_total += plot['file_size']
+        PLOTS_SIZE.set(plot_size_total)
+        farmed_stat = await client.get_farmed_amount()
+        farmed_amount = farmed_stat['farmed_amount']
+        FARMED_AMOUNT.set(farmed_amount)
+        farmed_last_height = farmed_stat['last_height_farmed']
+        FARMED_LAST.set(farmed_last_height)
+
+        seconds = 0
+        if blockchain is not None and plots is not None:
+            proportion = plot_size_total / blockchain["space"] if blockchain["space"] else -1
+            seconds = int((average_block_time) / proportion) if proportion else -1
+        TIME_TO_WIN.set(seconds)
+        client.close()
+        client_node.close()
+        client_harvester.close()
+    except:
+        print('error connecting to something')
+
+
+
+
+
+
+
+
+if __name__ == '__main__':
+    # Start up the server to expose the metrics.
+    start_http_server(8000)
+    # Generate some requests.
+    while True:
+        asyncio.run(main())
+        time.sleep(15)
